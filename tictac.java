cec4489abc9cd4a851415c31b640fb17265a7be4
VarKhwezi <139923193+VarKhwezi@users.noreply.github.com>
2024-06-21 10:59:35 +0000

    Update tictac.java

--- tictac.java
+++ tictac.java
@@ -2,9 +2,9 @@ public class TicTacToe{
 	public static void main(String[] args){
 	
 	char[][] gameBoard = {{' ','|',' ','|',' '},
-		{'_','+','_','+','_'}
-		{' ','|',' ','|',' '}
-		{'_','+','_','+','_'}
+		{'_','+','_','+','_'},
+		{' ','|',' ','|',' '},
+		{'_','+','_','+','_'},
  		{' ','|',' ','|',' '}};
 	
 	printGameBoard(gameBoard);
@@ -12,7 +12,17 @@ public class TicTacToe{
 	Scanner scan = new Scanner(System.in);
 	System.out.println(" " );
 	scan.nextInt();
-
+	System.out.printlin(pos);
 	}	
-}
 
+
+	public static void printGameBoard(){
+		for(char[] row: gameBoard){
+			for(char c : row ){
+				System.out.print(c)
+			}
+			System .out.print(c)
+		}
+		
+	}
+}
